08ab0e331c542628be3a2cb292afdfdcff19fd5a
yudsetiwann <yudsetiwann@gmail.com>
2026-05-09 22:07:45 +0700

    update

--- app/studio/[[...tool]]/page.tsx
+++ app/studio/[[...tool]]/page.tsx
@@ -8,13 +8,14 @@
  */
 "use client";
 
-import { NextStudio } from 'next-sanity/studio'
-import config from '../../../sanity.config'
+import dynamic from "next/dynamic";
+import config from "../../../sanity.config";
 
-export const dynamic = 'force-static'
-
-export { metadata, viewport } from 'next-sanity/studio'
+const NextStudio = dynamic(
+  () => import("next-sanity/studio").then((mod) => mod.NextStudio),
+  { ssr: false },
+);
 
 export default function StudioPage() {
-  return <NextStudio config={config} />
+  return <NextStudio config={config} />;
 }
